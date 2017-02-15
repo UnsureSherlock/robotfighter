3d40dab814b7698a8522ad18f5d294796cec554b
Griffin Nozell <griffin.nozell@plexxi.com>
2017-02-14 22:37:13 -0500

    Camera Movements, Slippery walls
    Camera follows players (not perfectly) and players cannot hold onto
    invisible walls anymore.

---
+++ robotfighter/Assets/Scripts/CameraFollower.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour {
+
+	public Transform ent1;
+	public Transform ent2;
+
+	private float dampTime = 0.5f;
+	private float lowerLimit = -4f;
+	private float startsize;
+	private Vector3 velocity = Vector3.zero;
+	private Camera thiscam;
+
+	private Vector3 midpoint;
+
+	// Use this for initialization
+	void Start () {
+		thiscam = GetComponent<Camera> ();
+		startsize = thiscam.orthographicSize;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (ent1 != null && ent2 != null) {
+			// find mid point then lerp to there and size camera
+
+			midpoint = ((ent1.position - ent2.position)/2f) + ent2.position; // finds position between two
+
+			Vector3 point = thiscam.WorldToViewportPoint(midpoint);
+			Vector3 delta = midpoint - thiscam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); 
+			Vector3 destination = transform.position + delta;
+
+			if (destination.y < lowerLimit) {
+				destination.y = lowerLimit;
+			}
+			transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+
+
+			if (thiscam.orthographic)
+			{
+				// The camera's forward vector is irrelevant, only this size will matter
+				if (midpoint.x > startsize) {
+					thiscam.orthographicSize = midpoint.x;
+				} else if (midpoint.x < -startsize) {
+					thiscam.orthographicSize = -midpoint.x;
+				} else {
+					thiscam.orthographicSize = startsize;
+				}
+			}
+			
+		} else if (ent1 != null) {
+			//lerp to ent1 
+			Vector3 point = thiscam.WorldToViewportPoint(ent1.position);
+			Vector3 delta = ent1.position - thiscam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); 
+			Vector3 destination = transform.position + delta;
+
+			if (destination.y < lowerLimit) {
+				destination.y = lowerLimit;
+			}
+			transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+
+
+			
+		} else if (ent2 != null) {
+			// lerp to ent2
+			Vector3 point = thiscam.WorldToViewportPoint(ent2.position);
+			Vector3 delta = ent2.position - thiscam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z));
+			Vector3 destination = transform.position + delta;
+
+			if (destination.y < lowerLimit) {
+				destination.y = lowerLimit;
+			}
+			transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+
+		
+		} else {
+			// nothing to focus on
+		}
+
+
+
+
+		
+	}
+}
---
+++ robotfighter/Assets/Scripts/Fighter_Fatso.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fighter_Fatso : Fighter {
+	
+	private float speed = 40f;
+	private float cSpeed;
+	private float maxSpeed = 10f;
+	private float jumpSpeed = 10f;
+
+	private bool on_ground_bool = true;
+
+	private bool invul = false;
+	private float invul_timer = 2f;
+	public SpriteRenderer playerSprite;
+	private Rigidbody playerRb;
+
+	// Use this for initialization
+	void Start () {
+		cSpeed = speed;
+		invul = false;
+		playerRb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (invul) {
+			invul_timer -= Time.deltaTime;
+			if (invul_timer < 0) {
+				invul = false;
+				invul_timer = 2f;
+			}
+		}
+	}
+
+	public void freeze(){
+
+	}
+
+
+	public void unfreeze(){
+
+	}
+
+	void FixedUpdate () {
+
+		// Get Hor and Vert inputs
+		float hor = Input.GetAxis ("Horizontal");
+		float vert = Input.GetAxis ("Vertical");
+
+
+		// Adds speed to bot
+		if (hor > 0) {
+			playerRb.AddForce (new Vector2 (cSpeed, 0));
+		} else if (hor < 0) {
+			playerRb.AddForce (new Vector2 (-cSpeed, 0));
+		} else {
+			playerRb.velocity.Set (0, 0, 0);
+		}
+
+		if ( !((transform.eulerAngles.z < 16) || (transform.eulerAngles.z > 344)) ) {
+			playerRb.AddForce (new Vector2 (cSpeed, 0));
+		}
+
+		// Handles MAX speed
+		if (Mathf.Abs(playerRb.velocity.x) > maxSpeed) {
+			playerRb.AddForce (new Vector2 (-hor * cSpeed, 0));
+		}
+
+		// Jumping of the Bot
+		if ((vert > 0)&&(on_ground())) {
+			playerRb.AddForce(new Vector2(0,jumpSpeed), ForceMode.Impulse);
+			on_ground_bool = false;
+		}
+
+	}
+
+	void OnCollisionStay(Collision col){
+		if (col.gameObject.transform.position.y < gameObject.transform.position.y) {
+			if (col.gameObject.tag == "Ground") {
+				on_ground_bool = true;
+			}
+		}
+	}
+
+	void OnCollisionExit(Collision col){
+		if (col.gameObject.tag == "Ground") {
+			on_ground_bool = false;
+		}
+	}
+
+	override public float get_total_health (){
+		return 100f;
+	}
+
+	override public bool on_ground (){
+		return on_ground_bool;
+	}
+}
